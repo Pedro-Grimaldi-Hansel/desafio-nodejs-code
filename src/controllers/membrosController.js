e64eb1e9b8f7fd6d9180c1d970da70255734f26d
Pedro Grimaldi <pedroghansel@gmail.com>
2022-09-23 19:09:32 -0300

    get , get por id, put e post funcionando

--- src/controllers/membrosController.js
+++ src/controllers/membrosController.js
@@ -7,6 +7,19 @@ class MembroController{
             res.status(200).json(membros)
         })
     }
+    
+    static listarMembroPorId = (req, res) => {      //exibe todos os membros
+        const id = req.params.id;
+
+        membros.findById(id,(err,membros)=>{
+            if(err){
+                res.status(400).send({message: `${err.message} - Id do livro não localizado!`})
+            }
+            else{
+                res.status(200).json(membros);
+            }
+        })
+    }
 
     static cadastrarMembro = (req,res) => {
         let livro = new membros(req.body);
@@ -21,6 +34,18 @@ class MembroController{
         })
     }
 
+    static atualizarMembro = (req,res) => {
+        const id = req.params.id;
+
+        membros.findByIdAndUpdate(id,{$set: req.body}, (err) =>{
+            if(!err){
+                res.status(200).send({message:'Membro atualizado!'})
+            }
+            else{
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
 }
 
 export default MembroController
--- src/routes/membrosRoutes.js
+++ src/routes/membrosRoutes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 router
     .get("/membros",Membrocontroller.listarMembros)
+    .get("/membros/:id", Membrocontroller.listarMembroPorId)
 
     .post("/membros", Membrocontroller.cadastrarMembro)
+
+    .put("/membros/:id", Membrocontroller.atualizarMembro)
 
 export default router;
